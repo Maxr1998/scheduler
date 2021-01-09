bf8d207b4ff2b77cda165311afd3d5b9831e1c47
Maxr1998 <max.rumpf1998@gmail.com>
2021-01-09 12:32:00 +0100

    Register database instance for injection

--- src/main/kotlin/de/uaux/scheduler/AppModule.kt
+++ src/main/kotlin/de/uaux/scheduler/AppModule.kt
@@ -1,11 +1,21 @@
 package de.uaux.scheduler
 
+import com.squareup.sqldelight.db.SqlDriver
+import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
 import de.uaux.scheduler.controller.NavigationController
+import de.uaux.scheduler.model.Database
+import de.uaux.scheduler.util.Constants
 import de.uaux.scheduler.util.LocalizationUtil
 import org.koin.dsl.module
 
 val appModule = module {
+    // Controllers
     single { NavigationController() }
 
+    // Database
+    single<SqlDriver> { JdbcSqliteDriver(Constants.DB_URL) }
+    single { Database(get()) }
+
+    // Utils
     single { LocalizationUtil("strings") }
 }
---
+++ src/main/kotlin/de/uaux/scheduler/util/Constants.kt
@@ -0,0 +1,5 @@
+package de.uaux.scheduler.util
+
+object Constants {
+    const val DB_URL = "jdbc:sqlite:scheduler.db"
+}
