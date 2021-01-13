7735d3da93630a7ac5d7711dbe889392c9def527
Maxr1998 <max.rumpf1998@gmail.com>
2021-01-13 16:32:05 +0100

    Add empty repository classes

--- src/main/kotlin/de/uaux/scheduler/AppModule.kt
+++ src/main/kotlin/de/uaux/scheduler/AppModule.kt
@@ -4,6 +4,8 @@ import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
 import de.uaux.scheduler.controller.NavigationController
 import de.uaux.scheduler.model.Database
+import de.uaux.scheduler.repository.EventRepository
+import de.uaux.scheduler.repository.StudycourseRepository
 import de.uaux.scheduler.util.Constants
 import de.uaux.scheduler.util.LocalizationUtil
 import org.koin.dsl.module
@@ -11,6 +13,10 @@ import org.koin.dsl.module
 val appModule = module {
     // Controllers
     single { NavigationController() }
+
+    // Repositories
+    single { StudycourseRepository(get()) }
+    single { EventRepository(get()) }
 
     // Database
     single<SqlDriver> { JdbcSqliteDriver(Constants.DB_URL) }
---
+++ src/main/kotlin/de/uaux/scheduler/repository/StudycourseRepository.kt
@@ -0,0 +1,5 @@
+package de.uaux.scheduler.repository
+
+import de.uaux.scheduler.model.Database
+
+class StudycourseRepository(database: Database)
