a4bfb9e4e91e2ce309d3624768eef1b2bedc22a0
Maxr1998 <max.rumpf1998@gmail.com>
2021-01-16 18:41:18 +0100

    Add insert and query methods for studycourses

--- src/main/kotlin/de/uaux/scheduler/repository/StudycourseRepository.kt
+++ src/main/kotlin/de/uaux/scheduler/repository/StudycourseRepository.kt
@@ -1,5 +1,36 @@
 package de.uaux.scheduler.repository
 
+import com.squareup.sqldelight.runtime.coroutines.asFlow
+import com.squareup.sqldelight.runtime.coroutines.mapToList
 import de.uaux.scheduler.model.Database
+import de.uaux.scheduler.model.Studycourse
+import de.uaux.scheduler.util.checkAndGetId
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 
-class StudycourseRepository(database: Database)
+class StudycourseRepository(database: Database) {
+    private val standardQueries = database.standardQueries
+    private val studycourseQueries = database.studycourseQueries
+
+    val allStudycoursesFlow: Flow<List<Studycourse>> =
+        studycourseQueries
+            .queryAll()
+            .asFlow()
+            .mapToList(Dispatchers.IO)
+
+    suspend fun insert(name: String, revision: String? = null): Long {
+        return withContext(Dispatchers.IO) {
+            studycourseQueries.transactionWithResult {
+                studycourseQueries.insert(name, revision)
+                standardQueries.checkAndGetId()
+            }
+        }
+    }
+
+    suspend fun queryAll(): List<Studycourse> {
+        return withContext(Dispatchers.IO) {
+            studycourseQueries.queryAll().executeAsList()
+        }
+    }
+}
