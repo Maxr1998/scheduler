732cfbeea83eb582b25679664c80886d5e0b5c93
Maxr1998 <max.rumpf1998@gmail.com>
2021-02-20 15:34:47 +0100

    Rework app colors

--- src/main/kotlin/de/uaux/scheduler/ui/AppContent.kt
+++ src/main/kotlin/de/uaux/scheduler/ui/AppContent.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import de.uaux.scheduler.controller.NavigationController
 import de.uaux.scheduler.ui.screens.EventManagementScreen
 import de.uaux.scheduler.ui.screens.HomeScreen
@@ -14,7 +15,19 @@ import org.koin.androidx.compose.get
 @Composable
 fun AppContent() {
     MaterialTheme(
-        colors = darkColors()
+        colors = darkColors(
+            primary = Color(0xFFFF9800),
+            primaryVariant = Color(0xFFC66900),
+            secondary = Color(0xFFBF360C),
+            background = Color(0xFF121212),
+            surface = Color(0xFF121212),
+            error = Color(0xFFDD2C00),
+            onPrimary = Color.Black,
+            onSecondary = Color.White,
+            onBackground = Color.White,
+            onSurface = Color.White,
+            onError = Color.White,
+        )
     ) {
         Row {
             val navigationController: NavigationController = get()
