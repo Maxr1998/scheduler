cb48c044f02af496b0223f5510b04e6918a8fd4d
Maxr1998 <max.rumpf1998@gmail.com>
2021-02-23 13:06:20 +0100

    Improve scrollbar color for dark mode

--- src/main/kotlin/de/uaux/scheduler/ui/AppContent.kt
+++ src/main/kotlin/de/uaux/scheduler/ui/AppContent.kt
@@ -1,13 +1,17 @@
 package de.uaux.scheduler.ui
 
+import androidx.compose.foundation.ScrollbarStyleAmbient
+import androidx.compose.foundation.defaultScrollbarStyle
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.darkColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Providers
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.compositeOver
 import de.uaux.scheduler.controller.NavigationController
 import de.uaux.scheduler.ui.screens.EventManagementScreen
 import de.uaux.scheduler.ui.screens.HomeScreen
@@ -32,17 +36,27 @@ fun AppContent() {
             onError = Color.White,
         )
     ) {
-        Row {
-            val navigationController: NavigationController = get()
-            Sidebar(
-                screenState = navigationController.currentScreen,
-            )
-            Surface(modifier = Modifier.fillMaxSize()) {
-                when (navigationController.currentScreen.value) {
-                    NavigationController.Screen.Home -> HomeScreen()
-                    NavigationController.Screen.Events -> EventManagementScreen()
-                    NavigationController.Screen.Timetable -> TimetableScreen()
-                    NavigationController.Screen.Settings -> SettingsScreen()
+        val scrollbarColor = with(MaterialTheme.colors) {
+            onSurface.copy(alpha = 0.2f).compositeOver(surface)
+        }
+        Providers(
+            ScrollbarStyleAmbient provides defaultScrollbarStyle().copy(
+                hoverColor = scrollbarColor,
+                unhoverColor = scrollbarColor,
+            ),
+        ) {
+            Row {
+                val navigationController: NavigationController = get()
+                Sidebar(
+                    screenState = navigationController.currentScreen,
+                )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    when (navigationController.currentScreen.value) {
+                        NavigationController.Screen.Home -> HomeScreen()
+                        NavigationController.Screen.Events -> EventManagementScreen()
+                        NavigationController.Screen.Timetable -> TimetableScreen()
+                        NavigationController.Screen.Settings -> SettingsScreen()
+                    }
                 }
             }
         }
