848963ec07ffe89b94ac6094210a2fdc6b1ee176
Maxr1998 <max.rumpf1998@gmail.com>
2021-02-23 15:30:09 +0100

    Update Kotlin to 1.4.30

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,6 +1,6 @@
 object Dependencies {
     object Versions {
-        const val kotlin = "1.4.21-2"
+        const val kotlin = "1.4.30"
         const val composePlugin = "0.3.0-build146"
         const val sqlDelight = "1.4.4"
         const val dependencyUpdatesPlugin = "0.36.0"
