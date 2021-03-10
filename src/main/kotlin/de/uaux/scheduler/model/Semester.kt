203ded47f5b6199301946e8b97d0e9abeef34d0b
Maxr1998 <max.rumpf1998@gmail.com>
2021-03-10 13:11:40 +0100

    Implement toString method for Semester

--- src/main/kotlin/de/uaux/scheduler/model/Semester.kt
+++ src/main/kotlin/de/uaux/scheduler/model/Semester.kt
@@ -3,6 +3,16 @@ package de.uaux.scheduler.model
 inline class Semester /*private*/ constructor(val code: Int) {
     constructor(type: Type, year: Int) : this(year * 10 + type.code)
 
+    override fun toString(): String {
+        val typeInt = code % 10
+
+        check(typeInt == 1 || typeInt == 2)
+
+        val type = Type.values()[typeInt - 1]
+        val year = code / 10
+        return "$type $year${if (type == Type.WS) "/${year + 1}" else ""}"
+    }
+
     enum class Type {
         SS, WS;
 
