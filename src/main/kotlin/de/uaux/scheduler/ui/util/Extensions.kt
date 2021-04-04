b96b7c494d884e1ef1db3892cc5345e4329b08a8
Maxr1998 <max.rumpf1998@gmail.com>
2021-04-04 12:24:17 +0200

    Add border modifier for debugging

--- src/main/kotlin/de/uaux/scheduler/ui/util/Extensions.kt
+++ src/main/kotlin/de/uaux/scheduler/ui/util/Extensions.kt
@@ -1,7 +1,16 @@
 package de.uaux.scheduler.ui.util
 
+import androidx.compose.foundation.border
 import androidx.compose.material.Colors
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.compositeOver
+import androidx.compose.ui.unit.Dp
 
 val Colors.highlight
     get() = onSurface.copy(alpha = 0.02f).compositeOver(surface)
+
+fun Modifier.debugBorder(): Modifier =
+    border(Dp.Hairline, SolidColor(Color.Red), RectangleShape)
