e985eed861b2b86dbd53a0c0fd666f8f9f9c2a43
Maxr1998 <max.rumpf1998@gmail.com>
2021-04-09 20:50:39 +0200

    Add methods to add/remove events to/from a Studycourse

--- src/main/kotlin/de/uaux/scheduler/repository/StudycourseRepository.kt
+++ src/main/kotlin/de/uaux/scheduler/repository/StudycourseRepository.kt
@@ -3,16 +3,20 @@ package de.uaux.scheduler.repository
 import com.squareup.sqldelight.runtime.coroutines.asFlow
 import com.squareup.sqldelight.runtime.coroutines.mapToList
 import de.uaux.scheduler.model.Database
+import de.uaux.scheduler.model.Event
 import de.uaux.scheduler.model.Studycourse
+import de.uaux.scheduler.model.dto.StudycourseEvent
 import de.uaux.scheduler.util.changedOne
 import de.uaux.scheduler.util.checkAndGetId
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
+import de.uaux.scheduler.model.StudycourseEvent as DbStudycourseEvent
 
 class StudycourseRepository(database: Database) {
     private val standardQueries = database.standardQueries
     private val studycourseQueries = database.studycourseQueries
+    private val studycourseEventQueries = database.studycourseEventQueries
 
     val allStudycoursesFlow: Flow<List<Studycourse>> =
         studycourseQueries
@@ -33,6 +37,24 @@ class StudycourseRepository(database: Database) {
             // Failed update or new object, insert
             studycourseQueries.insert(studycourse.name, studycourse.revision)
             standardQueries.checkAndGetId()
+        }
+    }
+
+    /**
+     * Links the [Event] in [studycourseEvent] to [studycourse], replacing all attributes with the provided ones
+     */
+    suspend fun link(studycourse: Studycourse, studycourseEvent: StudycourseEvent): Boolean = withContext(Dispatchers.IO) {
+        val relationObject = DbStudycourseEvent(studycourse.id, studycourseEvent.event.id, studycourseEvent.semester, studycourseEvent.required)
+        studycourseEventQueries.insertOrReplace(relationObject)
+        standardQueries.changedOne()
+    }
+
+    /**
+     * Unlinks [event] from [studycourse], removing all attached metadata
+     */
+    suspend fun unlink(studycourse: Studycourse, event: Event) {
+        withContext(Dispatchers.IO) {
+            studycourseEventQueries.delete(studycourse.id, event.id)
         }
     }
 }
