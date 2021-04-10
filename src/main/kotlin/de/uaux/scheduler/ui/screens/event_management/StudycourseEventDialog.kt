32ecbb8017cbe65c0e5a52dfe58ed6ee914d96a8
Maxr1998 <max.rumpf1998@gmail.com>
2021-04-11 00:06:13 +0200

    Fully implement dialog to add events to a studycourse and change metadata

--- src/main/kotlin/de/uaux/scheduler/ui/screens/event_management/StudycourseEventDialog.kt
+++ src/main/kotlin/de/uaux/scheduler/ui/screens/event_management/StudycourseEventDialog.kt
@@ -1,15 +1,43 @@
 package de.uaux.scheduler.ui.screens.event_management
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.CheckCircle
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import de.uaux.scheduler.model.Event
 import de.uaux.scheduler.model.Studycourse
 import de.uaux.scheduler.model.dto.StudycourseEvent
+import de.uaux.scheduler.repository.EventRepository
 import de.uaux.scheduler.repository.StudycourseRepository
+import de.uaux.scheduler.ui.util.LabeledTextField
 import de.uaux.scheduler.ui.util.PopupDialog
+import de.uaux.scheduler.ui.util.SaveButton
+import de.uaux.scheduler.ui.util.SearchableSelectionDropdown
 import de.uaux.scheduler.ui.util.l
 import kotlinx.coroutines.launch
 import org.koin.androidx.compose.get
@@ -17,6 +45,18 @@ import org.koin.androidx.compose.get
 @Composable
 fun StudycourseEventDialog(studycourse: Studycourse, studycourseEvent: StudycourseEvent?, onDismissRequest: () -> Unit) {
     val studycourseRepository: StudycourseRepository = get()
+    val eventRepository: EventRepository = get()
+    val event = remember { mutableStateOf(studycourseEvent?.event) }
+    val semesterText = remember { mutableStateOf(TextFieldValue(studycourseEvent?.semester?.toString().orEmpty())) }
+    val semester = semesterText.value.text.trim().toIntOrNull()
+    val semesterError = when {
+        semesterText.value.text.isBlank() -> null
+        semester == null -> l("input_error_only_numbers")
+        semester !in 1..10 -> l("input_error_invalid_semester")
+        else -> null
+    }
+
+    val required = remember { mutableStateOf(studycourseEvent?.required ?: false) }
 
     PopupDialog(
         title = l(if (studycourseEvent == null) "dialog_title_add_studycourse_event" else "dialog_title_edit_studycourse_event"),
@@ -44,6 +84,103 @@ fun StudycourseEventDialog(studycourse: Studycourse, studycourseEvent: Studycour
             ) {
                 Text(text = l("button_text_cancel"))
             }
+
+            SaveButton(
+                enabled = event.value != null && semesterError == null,
+                onSave = {
+                    coroutineScope.launch {
+                        val updated = StudycourseEvent(
+                            requireNotNull(event.value),
+                            semester,
+                            required.value,
+                        )
+                        studycourseRepository.link(studycourse, updated)
+                        onDismissRequest()
+                    }
+                }
+            )
         }
-    ) {}
+    ) {
+        Column {
+            if (studycourseEvent == null) { // Select new event
+                val searchQuery = remember { mutableStateOf("") }
+
+                val events: List<Event> by produceState(emptyList(), searchQuery.value) {
+                    value = eventRepository.searchByName(searchQuery.value, studycourse)
+                }
+
+                SearchableSelectionDropdown(
+                    label = l("input_label_studycourse_event_event"),
+                    onSearch = { query ->
+                        event.value = null
+                        searchQuery.value = query
+                    },
+                    searchResults = events,
+                    onSelect = { selectedEvent ->
+                        event.value = selectedEvent
+                    },
+                    selected = event.value,
+                    itemLabel = Event::name
+                ) { event ->
+                    Text(text = event.name)
+                }
+            } else { // Event is already selected, show read-only
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    label = {
+                        Text(text = l("input_label_studycourse_event_event"))
+                    },
+                    value = studycourseEvent.event.name,
+                    onValueChange = {},
+                    readOnly = true,
+                    singleLine = true,
+                    trailingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.CheckCircle,
+                            contentDescription = null,
+                        )
+                    },
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            LabeledTextField(
+                text = semesterText,
+                label = l("input_label_studycourse_event_semester"),
+                placeholder = l("input_hint_studycourse_event_semester"),
+                semesterError,
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            val borderColor = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.disabled)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp)
+                    .border(1.dp, SolidColor(borderColor), MaterialTheme.shapes.medium)
+                    .clip(MaterialTheme.shapes.medium)
+                    .clickable {
+                        required.value = !required.value
+                    }
+                    .padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    modifier = Modifier.weight(1f),
+                    text = l(if (required.value) "studycourse_domain_required" else "studycourse_domain_voluntary"),
+                )
+
+                Switch(
+                    checked = required.value,
+                    onCheckedChange = { checked ->
+                        required.value = checked
+                    }
+                )
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+    }
 }
