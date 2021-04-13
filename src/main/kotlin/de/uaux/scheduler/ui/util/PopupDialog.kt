712414d7f25f83925761f38e26a3922d2e7e1aa1
Maxr1998 <max.rumpf1998@gmail.com>
2021-04-13 21:50:55 +0200

    Apply scrim to dialogs

--- src/main/kotlin/de/uaux/scheduler/ui/util/PopupDialog.kt
+++ src/main/kotlin/de/uaux/scheduler/ui/util/PopupDialog.kt
@@ -1,16 +1,19 @@
 package de.uaux.scheduler.ui.util
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.DrawerDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -22,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onKeyEvent
@@ -36,57 +40,63 @@ fun PopupDialog(
     content: @Composable BoxScope.() -> Unit,
 ) {
     Popup(
-        alignment = Alignment.Center,
         isFocusable = true,
         onDismissRequest = onDismissRequest,
     ) {
-        val dialogFocusRequester = remember { FocusRequester() }
-        Surface(
+        Box(
             modifier = Modifier
-                .width(560.dp)
-                .focusRequester(dialogFocusRequester)
-                .focusModifier()
-                .onKeyEvent { keyEvent ->
-                    val isEscape = keyEvent.key == Key.Escape
-                    if (isEscape) {
-                        onDismissRequest?.invoke()
-                    }
-                    isEscape
-                },
-            shape = RoundedCornerShape(8.dp),
-            elevation = 2.dp,
+                .fillMaxSize()
+                .background(Color.Black.copy(alpha = DrawerDefaults.ScrimOpacity)),
+            contentAlignment = Alignment.Center,
         ) {
-            Column {
-                if (title != null) {
-                    Box(
-                        modifier = Modifier.padding(horizontal = 24.dp).height(64.dp),
-                        contentAlignment = Alignment.CenterStart,
-                    ) {
-                        Text(
-                            text = title,
-                            style = MaterialTheme.typography.h6,
-                        )
+            val dialogFocusRequester = remember { FocusRequester() }
+            Surface(
+                modifier = Modifier
+                    .width(560.dp)
+                    .focusRequester(dialogFocusRequester)
+                    .focusModifier()
+                    .onKeyEvent { keyEvent ->
+                        val isEscape = keyEvent.key == Key.Escape
+                        if (isEscape) {
+                            onDismissRequest?.invoke()
+                        }
+                        isEscape
+                    },
+                shape = RoundedCornerShape(8.dp),
+                elevation = 24.dp,
+            ) {
+                Column {
+                    if (title != null) {
+                        Box(
+                            modifier = Modifier.padding(horizontal = 24.dp).height(64.dp),
+                            contentAlignment = Alignment.CenterStart,
+                        ) {
+                            Text(
+                                text = title,
+                                style = MaterialTheme.typography.h6,
+                            )
+                        }
                     }
-                }
 
-                Box(
-                    modifier = Modifier.padding(horizontal = 24.dp),
-                ) {
-                    content()
-                }
+                    Box(
+                        modifier = Modifier.padding(horizontal = 24.dp),
+                    ) {
+                        content()
+                    }
 
-                Row(
-                    modifier = Modifier.height(52.dp).fillMaxWidth().padding(8.dp),
-                    horizontalArrangement = Arrangement.End,
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    actions()
+                    Row(
+                        modifier = Modifier.height(52.dp).fillMaxWidth().padding(8.dp),
+                        horizontalArrangement = Arrangement.End,
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        actions()
+                    }
                 }
             }
-        }
 
-        SideEffect {
-            dialogFocusRequester.requestFocus()
+            SideEffect {
+                dialogFocusRequester.requestFocus()
+            }
         }
     }
 }
