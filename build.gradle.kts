b6121da04cfda6d78b529a8a394942d6d55ff260
Maxr1998 <max.rumpf1998@gmail.com>
2021-04-24 18:14:56 +0200

    Fix crash in native package
    Native packages crash because of missing Java module for logback

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,6 +46,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = project.name
+            modules("java.naming")
         }
     }
 }
